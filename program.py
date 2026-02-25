89a4b560daf7dbaf40c691cf3fd58f062f9ad202
chandbabu47 <mr9093977@gmail.com.com>
2026-02-25 14:19:38 +0500

    Multiplication program.

---
+++ program.py
@@ -0,0 +1,8 @@
+# Write a program that get two integer values from the user amd multiply to output result.
+
+
+num1=int(input("Enter your first number"))
+num2=int(input("Enter your second number"))
+result = num1 * num2
+
+print("The answer of multiplication is:",result)
